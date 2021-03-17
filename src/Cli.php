4f5a536691cb453d7a91725f8865652ecb8fe1eb
poymanov <n.poymanov@gmail.com>
2021-03-17 21:25:57 +0300

    Перенос логики игры в отдельный файл

---
+++ src/Cli.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brain\Games\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function run()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
