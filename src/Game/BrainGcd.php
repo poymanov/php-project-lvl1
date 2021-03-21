48ddd3aa36a610c479302e356c2f36f4b6d67a31
poymanov <n.poymanov@gmail.com>
2021-03-21 17:08:32 +0300

    Рефакторинг игры "НОД"

--- src/Game/BrainGcd.php
+++ src/Game/BrainGcd.php
@@ -10,40 +10,24 @@ use function cli\line;
 
 function run()
 {
-    Engine\printWelcomeMessage();
-
-    $name = Engine\askName();
-
-    printGameRules();
-
-    $successAttempts = 0;
-
-    for ($i = 1; $i <= Engine\MAX_ATTEMPTS; $i++) {
+    Engine\play(getGameRules(), function () {
         $firstInteger  = random_int(Engine\MIN_INT, Engine\MAX_INT);
         $secondInteger = random_int(Engine\MIN_INT, Engine\MAX_INT);
 
         printQuestion($firstInteger, $secondInteger);
 
-        $answer = Engine\askAnswer();
-
-        $correctResult = getCorrectResult($firstInteger, $secondInteger);
-
-        if (Engine\isAnswerCorrect($correctResult, $answer, $name)) {
-            $successAttempts++;
-        }
-    }
-
-    Engine\printGameResult($successAttempts, $name);
+        return getCorrectResult($firstInteger, $secondInteger);
+    });
 }
 
-function printGameRules()
+function getGameRules()
 {
-    line('Find the greatest common divisor of given numbers.');
+    return 'Find the greatest common divisor of given numbers.';
 }
 
 function printQuestion($firstInteger, $secondInteger)
 {
-    line("Question: %s", "$firstInteger, $secondInteger");
+    line("Question: %s", "$firstInteger $secondInteger");
 }
 
 function getCorrectResult($firstInteger, $secondInteger)
